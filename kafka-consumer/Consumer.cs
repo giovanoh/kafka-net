c0cc0714e13e3e7b5531eac3fa062802e0f4a6f3
giovano <giovano@priori.com.br>
2024-04-22 16:40:37 -0300

    Arquivos iniciais do projeto

---
+++ kafka-consumer/Consumer.cs
@@ -0,0 +1,40 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+
+public class Consumer {
+
+  public static void Main(string[] args) {
+    if (args.Length != 1) {
+      Console.WriteLine("Please provide the configuration file path as a command line argument");
+    }
+
+    IConfiguration configuration = new ConfigurationBuilder()
+      .AddIniFile(args[0])
+      .Build();
+
+    configuration["group.id"] = "my-consumer-group";
+    configuration["auto.offset.reset"] = "earliest";
+
+    const string topic = "order_details";
+
+    CancellationTokenSource cts = new CancellationTokenSource();
+    Console.CancelKeyPress += (_, e) => {
+      e.Cancel = true; // prevent the process from terminating.
+      cts.Cancel();
+    };
+
+    using var consumer = new ConsumerBuilder<string, string>(configuration.AsEnumerable()).Build();
+    consumer.Subscribe(topic);
+    try {
+      while (true) {
+        var cr = consumer.Consume(cts.Token);
+        Console.WriteLine($"Consumed event from topic {topic}: key = {cr.Message.Key,-10} value = {cr.Message.Value}");
+      }
+    } catch(OperationCanceledException) {
+      // Ctrl-C was pressed.
+    } finally {
+      consumer.Close();
+    }
+  }
+
+}
